4391cededa371e953fb7dfd5bec68321ba34bf83
Roman Smolins <romasmolin29@gmail.com>
2024-06-19 22:56:24 +0300

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        API_URL: 'https://kiwi-be.vercel.app/',
+    },
     images: {
         domains: ['res.cloudinary.com']
     }
