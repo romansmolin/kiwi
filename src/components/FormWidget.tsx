9bd32340bf56abfa0161a6b54e2a3cc883f76f63
Romans Smolins <romans.smolins@fintecor.com>
2024-09-20 11:30:26 +0300

    Rebuild modal

--- src/components/FormWidget.tsx
+++ src/components/FormWidget.tsx
@@ -6,11 +6,7 @@ const FormWidget: React.FC = () => {
 
     return (
         <Link
-            href={{
-                query: {
-                    showModal: true
-                }
-            }}
+            href={{ query: { showModal: true } }}
             className='fixed z-10 flex justify-center h-16 w-16 p-2 border border-primary-400 bg-primary-100 border-dashed items-center bottom-7 right-7 lg:bottom-10 lg:right-10 rounded-full cursor-pointer'>
             <Logo className="h-16 w-16" />
         </Link>
